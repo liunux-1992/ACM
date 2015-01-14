621ffd8918e86c617eed938151ded862fdd72c12
liunuxer1992 <464065503@qq.com>
2015-01-15 07:04:30 +0800

    ACM
    ACM

---
+++ 1003/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: liumiao
+ *
+ * Created on 2015年1月14日, 下午11:38
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
